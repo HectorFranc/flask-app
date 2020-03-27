fd33e54e2a6ef89fd752892ee81a0d1ffdd90ca9
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-03-26 18:50:03 -0600

    Redirect and cookies

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr  # IP del usuario
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
 def hello():
-    user_ip = request.remote_addr  # Ip del usuario
+    user_ip = request.cookies.get('user_ip')
+
     return f'Your IP is: {user_ip}'
