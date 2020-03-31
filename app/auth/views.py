0e29b6dc7e9ab46616e2af2dd24411ec8c60d63f
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-03-31 15:11:04 -0600

    Logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,10 @@
 from flask import render_template, flash, make_response, url_for, redirect, session
+from flask_login import login_user, login_required, logout_user
 
 from app.forms import LoginForm
 from . import auth
+from app.firestore_service import get_user
+from app.models import UserData, UserModel
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -13,10 +16,36 @@ def login():
 
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
 
-        flash('Nombre de usuario registrado con exito')
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido')
+
+                redirect(url_for('hello'))
+            else:
+                flash('La información no coincide')
+        else:
+            flash('El usuario no existe')
 
         return make_response(redirect(url_for('index')))
 
     return render_template('login.html', **context)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa pronto')
+
+    return redirect(url_for('auth.login'))
